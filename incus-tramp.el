6c21d613d989c7f26838ec8034c637d5b37bbe55
Roman Isaev <mdnight@riseup.net>
2026-05-21 00:13:02 +0000

    add a docstring and a preamble

--- incus-tramp.el
+++ incus-tramp.el
@@ -6,8 +6,38 @@
 ;; Maintainer: Roman Isaev <mdnight@riseup.net>
 ;; Keywords: tramp, incus, containers, extensions
 ;; Package: incus-tramp
-;; Package-Version: 0.0.1
-;; Package-Requires: ((emacs "30.0"))
+;; Package-Version: 0.1.0
+;; Package-Requires: ((emacs "30.1"))
+
+;; This file is not part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Allows Tramp access to environments provided by Incus.
+;;
+;; ## Usage
+;;
+;; Open a file on a running Incus container:
+;;
+;;     C-x C-f /incus:CONTAINER:/path/to/file
+;;
+;; Where:
+;;     CONTAINER     is the container to connect to.
+
+;;; Code:
 
 (require 'tramp)
 (require 'subr-x)
