7bfca66aa5f3e18b98bc9ea3c1961716c4d1acdb
Roman Isaev <mdnight@riseup.net>
2026-05-23 14:18:45 +0000

    fix compile error

--- incus-tramp.el
+++ incus-tramp.el
@@ -64,21 +64,20 @@
                 (names (split-string raw-list "\n" 'omit)))
       (mapcar (lambda (name) (list nil name)) names))))
 
-;;;###autoload
 (defun incus-tramp-enable-method ()
   (with-eval-after-load 'tramp
     (add-to-list 'tramp-methods
-                 `(,incus-tramp-method
-                   (tramp-login-program ,incus-tramp-incus-program)
-                   (tramp-login-args (("exec") ("%h") (,(format "--env TERM=%s" tramp-terminal-type)) ("--") ("%l")))
-                   (tramp-direct-async (,tramp-default-remote-shell "-c"))
-                   (tramp-remote-shell ,tramp-default-remote-shell)
-                   (tramp-remote-shell-login ("-l"))
-                   (tramp-remote-shell-args ("-i" "-c"))
-                   (tramp-completion-use-cache nil)
-                   (tramp-connection-timeout 60)))
+                 (incus-tramp-method
+                  (tramp-login-program incus-tramp-incus-program)
+                  (tramp-login-args (("exec") ("%h") ((format "--env TERM=%s" tramp-terminal-type)) ("--") ("%l")))
+                  (tramp-direct-async (tramp-default-remote-shell "-c"))
+                  (tramp-remote-shell tramp-default-remote-shell)
+                  (tramp-remote-shell-login ("-l"))
+                  (tramp-remote-shell-args ("-i" "-c"))
+                  (tramp-completion-use-cache nil)
+                  (tramp-connection-timeout 60)))
     (tramp-set-completion-function incus-tramp-method
-                                   `((tramp-incus-completion-function ,incus-tramp-method)))
+                                   ((tramp-incus-completion-function incus-tramp-method)))
     (add-to-list 'tramp-completion-multi-hop-methods incus-tramp-method)))
 
 (provide 'incus-tramp)
